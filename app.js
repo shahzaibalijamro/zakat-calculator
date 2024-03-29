f4fde8eb309d910185caec94534b6262e660659e
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-29 20:24:43 +0500

    commands added

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 let input1 = document.querySelector(".cash");
 let input2 = document.querySelector(".gold");
-let input3 = document.querySelector(".gold-rate");
 let h1 = document.querySelector(".h1");
-
+let select = document.querySelector("#select");
 function calculateZakat() {
     let input11 = parseFloat(input1.value);
     let input12 = parseFloat(input2.value);
-    let input13 = parseFloat(input3.value);
-    let finalAnswer = (input13*input12)+input11;
-    let finalAnswer2 = finalAnswer/40;
-    if (finalAnswer > 952000) {
+    if (isNaN(input11)) {
+        input11 = 0;
+    }
+    let finalAnswer = select.value * input12 + input11;
+    let finalAnswer2 = finalAnswer / 40;
+    if (finalAnswer >= 1744500) {
         h1.innerHTML = "Your Zakat would be " + finalAnswer2;
-    }else{
+    } else {
         h1.innerHTML = "You are not eligible";
     }
 }
